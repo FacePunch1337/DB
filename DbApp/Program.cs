2bee41ecb0a730c05137e3064f373ab747cd11b9
FacePunch1337 <rezol1337@gmail.com>
2022-05-26 18:00:07 +0200

    Добавьте файлы проекта.

---
+++ DbApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
